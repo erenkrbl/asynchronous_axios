ebd2d96cb3f97d43f7cc9a29678d4463672348d3
Joseph Abraham <ekurubal24@gmail.com>
2021-04-21 16:34:03 +0300

    Added reject with catch

--- promise.js
+++ promise.js
@@ -1,13 +1,15 @@
-const myPromise = new Promise((resolve, rejeck) =>{
+const myPromise = new Promise((resolve, reject) =>{
     // long time operation
     setTimeout(()=>{
         //console.log("After 3 seconds, the transactions are finished.");
         // in case the process is running
         //resolve("After 3 seconds, the transactions are finished.");
-        resolve({sName : 'Eric', id : 234});
+        //resolve({sName : 'Eric', id : 234});
+        // in case of error
+        reject('I could not go to internet');
     },3000);
 });
 
-myPromise.then(result =>{
-    console.log(result);
-})
+myPromise
+    .then(result =>{console.log(result)})
+    .catch(error =>{console.log(error)});
