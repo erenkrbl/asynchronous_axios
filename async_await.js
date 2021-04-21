cc4cc0ac07112cea333245ae36ee87c16274dd51
Joseph Abraham <ekurubal24@gmail.com>
2021-04-21 19:31:39 +0300

    Added async and await function

--- async_await.js
+++ async_await.js
@@ -11,7 +11,7 @@ function getCourses(userName) {
     console.log(`Bring ${userName}'s courses`);
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            reject('Bring courses error out');
+            //reject('Bring courses error out');
             resolve(['python', 'javascript', 'flutter']);
         }, 2000);
     });
@@ -26,11 +26,19 @@ function getComment(coursesName){
     });
 }
 
-getUser(1234)
-    .then(user =>getCourses(user.fname))
-    .then(coursesArray => getComment(coursesArray[0]))
-    .then(comment => console.log(comment))
-    .catch(error => console.log("Error: " + error));
+commenstShow();
+async function commenstShow() {
+    const userObje = await getUser(3456);
+    const coursesArray = await getCourses(userObje.fname);
+    const comment = await getComment(coursesArray[0]);
+    console.log(comment);
+}
+
+// getUser(1234)
+//     .then(user =>getCourses(user.fname))
+//     .then(coursesArray => getComment(coursesArray[0]))
+//     .then(comment => console.log(comment))
+//     .catch(error => console.log("Error: " + error));
 
 // getUser(1234).then(user => {
 //     getCourses(user.fname).then(coursesArray => {
