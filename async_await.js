bdda1308c01805824755b2f9ad4fe7fd96ce1d01
Joseph Abraham <ekurubal24@gmail.com>
2021-04-21 18:58:58 +0300

    Added async_await example

---
+++ async_await.js
@@ -0,0 +1,39 @@
+function getUser(id){
+    console.log(`${id} id fetch user's information`);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({fname: 'Eric', id: id})
+        }, 2000);
+    });
+}
+
+function getCourses(userName) {
+    console.log(`Bring ${userName}'s courses`);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(['python', 'javascript', 'flutter']);
+        }, 2000);
+    });
+}
+
+function getComment(coursesName){
+    console.log(`Bring ${coursesName}'s comments`);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Courses wonderfull');
+        }, 2000);
+    });
+}
+
+getUser(1234).then(user => {
+    getCourses(user.fname).then(coursesArray => {
+        getComment(coursesArray[0]).then(comment =>{
+            console.log(comment);
+        })
+        console.log(coursesArray);
+    });
+});
+
+// getCourses('eric').then(coursesArray => {
+//     console.log(coursesArray);
+// });
