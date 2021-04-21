5be5e958bee0209cce4400b3fdeae03fd6992f94
Joseph Abraham <ekurubal24@gmail.com>
2021-04-21 19:21:47 +0300

    Added catch and reject

--- async_await.js
+++ async_await.js
@@ -11,6 +11,7 @@ function getCourses(userName) {
     console.log(`Bring ${userName}'s courses`);
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            reject('Bring courses error out');
             resolve(['python', 'javascript', 'flutter']);
         }, 2000);
     });
@@ -28,7 +29,8 @@ function getComment(coursesName){
 getUser(1234)
     .then(user =>getCourses(user.fname))
     .then(coursesArray => getComment(coursesArray[0]))
-    .then(comment => console.log(comment));
+    .then(comment => console.log(comment))
+    .catch(error => console.log("Error: " + error));
 
 // getUser(1234).then(user => {
 //     getCourses(user.fname).then(coursesArray => {
